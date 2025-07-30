15663eb2a756b990692aa23e0411bde15833c2e4
lenafo <34781774+lenafo@users.noreply.github.com>
2025-07-29 21:56:42 -0600

    Create app.js

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+// Importa GSAP para animaciones si quieres
+// import { gsap } from "gsap";
+
+// Ejemplo básico:
+// gsap.from('.woocommerce-loop-product__link', { opacity: 0, y: 20, stagger: 0.1 });
+
